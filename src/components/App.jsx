49a9d3e210708992b0ddfe2f479599ef5b19ffc7
Inna-Olkhovskaya <115504107+Inna-Olkhovskaya@users.noreply.github.com>
2023-05-17 22:32:33 +0300

    Finished markup + added styles
    1. Created markup for component
    2. Added styles as module.css

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,64 @@
-export const App = () => {
-  return (
-    <>
-      
-    </>
-  );
-};
+import React, { Component } from 'react';
+import Section from './Section/Section';
+import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
+import Statistics from './Statistics/Statistics';
+
+class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  // Метод-счетчик +1 по названию кнопки
+  onLeaveFeedback = e => {
+    const label = e.target.textContent;
+
+    this.setState(prevState => ({ [label]: (prevState[label] += 1) }));
+  };
+
+  // Метод-подсчет общего кол-ва отзывов
+  countTotalFeedback = () => {
+    const total = Object.keys(this.state).reduce(
+      (acc, value) => acc + this.state[value],
+      0
+    );
+
+    return total;
+  };
+
+  // % положительных отзывов
+  countPositiveFeedbackPercentage = () => {
+    const percent = Math.round(
+      (this.state.good * 100) / this.countTotalFeedback()
+    );
+
+    return percent;
+  };
+
+  render() {
+    const { good, neutral, bad } = this.state;
+
+    return (
+      <>
+        <Section title="Please leave feedback">
+          <FeedbackOptions
+            options={this.state}
+            onLeaveFeedback={this.onLeaveFeedback}
+          />
+        </Section>
+        <Section title="Statistics">
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={this.countTotalFeedback}
+            positivePercentage={this.countPositiveFeedbackPercentage}
+          />
+        </Section>
+      </>
+    );
+  }
+}
+
+export default App;
